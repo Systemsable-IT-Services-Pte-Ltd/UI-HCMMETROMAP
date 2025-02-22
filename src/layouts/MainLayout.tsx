087a1452ce26fd8946adea4a319295e7bdeb94b0
tgbaotechnoasia <tg.bao@technoasia.com.vn>
2025-02-22 14:32:10 +0700

    testing push devBao

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,8 +3,10 @@ import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
-    <div className="max-w-full min-h-screen bg-gray-100">
-      <Outlet />
+    <div className="flex flex-col max-w-full min-h-screen bg-gray-100">
+      <div className="max-w-full min-h-screen bg-amber-100">
+        <Outlet />
+      </div>
     </div>
   );
 };
