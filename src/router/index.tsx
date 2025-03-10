ca801547d713aad2231758a8017b42ec0bbcc716
PHUONGNAM101203 <pnamhuynhle@gmail.com>
2025-03-10 13:10:16 +0700

    add Map, Contact Page, fix route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,7 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import { Contact, About } from "@components/loadableComponent";
 import MainLayout from "@layouts/MainLayout";
 import ErrorLayout from "@layouts/ErrorLayout";
-import Home from "@pages/Home";
+// import Home from "@pages/Home";
+import Maps from "@pages/Maps";
 
 export const router = createBrowserRouter([
   {
@@ -12,7 +13,7 @@ export const router = createBrowserRouter([
     children: [
       {
         path: "/maps",
-        element: <Home />,
+        element: <Maps/>,
       },
       {
         path: "/contact",
